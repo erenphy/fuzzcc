3870eb2f7031993a26c3d89260602b5e5ff97fd9
erenphy <lixiaohui_ouc@163.com>
2023-01-18 23:22:39 -0800

    change ccgene_dir()

--- image/ccgenerator.py
+++ image/ccgenerator.py
@@ -31,40 +31,57 @@ class Generator(threading.Thread):
         init_size = randint(INIT_TIME_MIN, INIT_TIME_MAX)
         while init_size:
             # 每条测试用例的长度随机生成
-            print("\niniting time left: " + str(init_size))
+            print("\ninit seedpool countdown" + str(init_size))
             cur_queue = []
             cur_length = randint(1,self.length)
-            print("\ncur_length = " + str(cur_length))
+            # 0118:追踪过程中产生的新目录/文件，指导下一个文件操作的参数
+            cur_exist_file = []
+            # print("\ncur_length = " + str(cur_length))
             while cur_length:
                 rdm = randint(0, OPS_LENGTH - 1)
                 print("\nrandom ops = " + str(rdm))
+                syscall_kv = []
                 if rdm == ops.CREAT:
                     print("\ncreating files")
                     cur_op = 'creat'
                     cur_name = ccgene_file()
-                    syscall_kv = [cur_op, cur_name]
+                    cur_exist_file.append(cur_name)
+                    syscall_kv.append([cur_op, cur_name])
                     # ccsyscalls.cccreat(globalVar.MY_MNT_POINT, cur_para)
                 elif rdm == ops.APPEND:
                     cur_op = 'append'
-                    cur_name = ccgene_file()
+                    if cur_exist_file:
+                        cur_name = random.choice(cur_exist_file)
+                    else:
+                        cur_name = ccgene_file()
+                        cur_exist_file.append(cur_name)
+                    # cur_name = ccgene_file()
                     cur_para = ccgene_wt_para()
-                    syscall_kv = [cur_op, cur_name, cur_para]
+                    syscall_kv.append([cur_op, cur_name, cur_para])
                     # ccsyscalls.ccappend(globalVar.MY_MNT_POINT, cur_name, cur_para)
                 elif rdm == ops.HARDLN:
                     cur_op = 'hardlink'
                     cur_name1 = ccgene_file()
                     cur_name2 = ccgene_file()
-                    syscall_kv = [cur_op, [cur_name1, cur_name2]]
+                    # name1最好存在，不存在的话在syscall里会创建 这里就不判断了
+                    cur_exist_file.append(cur_name1)
+                    cur_exist_file.append(cur_name2)
+                    syscall_kv.append([cur_op, [cur_name1, cur_name2]])
                     # ccsyscalls.cchdlink(globalVar.MY_MNT_POINT, cur_name)
                 elif rdm == ops.MKDIR:
                     cur_op = 'mkdir'
                     cur_name = ccgene_dir()
-                    syscall_kv = [cur_op, cur_name]
+                    syscall_kv.append([cur_op, cur_name])
                     # ccsyscalls.ccmkdir(globalVar.MY_MNT_POINT, cur_name)
                 elif rdm == ops.READ:
                     cur_op = 'read'
-                    cur_name = ccgene_file()
-                    syscall_kv = [cur_op, cur_name]
+                    if cur_exist_file:
+                        # 读一些现有的文件
+                        cur_name = random.choice(cur_exist_file)
+                    else:
+                        cur_name = ccgene_file()
+                        cur_exist_file.append(cur_name)
+                    syscall_kv.append([cur_op, cur_name])
                     # ccsyscalls.ccread(globalVar.MY_MNT_POINT, cur_name)
                 elif rdm == ops.REMOVE:
                     cur_op = 'remove'
@@ -73,7 +90,7 @@ class Generator(threading.Thread):
                     # Q:返回值是空字符串怎么办
                     # A:还没想到
                     cur_name = random.choice(file_or_dir)()
-                    syscall_kv = [cur_op, cur_name]
+                    syscall_kv.append([cur_op, cur_name])
                     # ccsyscalls.ccremove(globalVar.MY_MNT_POINT, cur_name)
                 elif rdm == ops.RENAME:
                     cur_op = 'rename'
@@ -83,34 +100,57 @@ class Generator(threading.Thread):
                     cur_name3 = ccgene_dir()
                     cur_name4 = ccgene_dir()
                     cur_name = random.choice([[cur_name1, cur_name2], [cur_name3, cur_name4]])
-                    syscall_kv= [cur_op, cur_name]
+                    if cur_name[0] == cur_name1:
+                        cur_exist_file.append(cur_name2)
+                    syscall_kv.append([cur_op, cur_name])
                     # ccsyscalls.ccrename(globalVar.MY_MNT_POINT, cur_name1, cur_name2)
                 elif rdm == ops.SOFTLN:
                     cur_op = 'softlink'
-                    cur_name1 = ccgene_file()
+                    if cur_exist_file:
+                        cur_name1 = random.choice(cur_exist_file)
+                    else:
+                        cur_name1 = ccgene_file()
+                        cur_exist_file.append(cur_name1)
+                    # cur_name1 = ccgene_file()
                     cur_name2 = ccgene_file()
+                    cur_exist_file.append(cur_name2)
                     cur_name3 = ccgene_dir()
+                    # 可能是给目录建立软连接 也可能是文件
                     cur_name4 = random.choice([cur_name1, cur_name3])
                     cur_name = [cur_name4, cur_name2]
-                    syscall_kv = [cur_op, cur_name]
+                    syscall_kv.append([cur_op, cur_name])
                     # ccsyscalls.ccsflink(globalVar.MY_MNT_POINT,cur_name)
                 elif rdm == ops.SYNC:
                     cur_op = 'sync'
                     syscall_kv = [cur_op]
                 elif rdm == ops.WRITE:
                     cur_op = 'write'
-                    cur_name = ccgene_file()
+                    if cur_exist_file:
+                        cur_name = random.choice(cur_exist_file)
+                    else:
+                        cur_name = ccgene_file()
+                        cur_exist_file.append(cur_name)
+                    # cur_name = ccgene_file()
                     cur_para = ccgene_wt_para()
-                    syscall_kv = [cur_op, cur_name, cur_para]
+                    syscall_kv.append([cur_op, cur_name, cur_para])
                     # ccsyscalls.ccwrite(globalVar.MY_MNT_POINT, cur_name, cur_para)
-                elif rdm ==ops.FSYNC:
+                elif rdm == ops.FSYNC:
                     cur_op = 'fsync'
-                    cur_name = ccgene_file()
-                    syscall_kv = [cur_op, cur_name]
+                    if cur_exist_file:
+                        cur_name = random.choice(cur_exist_file)
+                        syscall_kv.append([cur_op, cur_name])
+                    else:
+                        pass
+                    # logging.debug(f"FSYNCing---cur_exist_file: {cur_exist_file}")
+                    # syscall_kv = [cur_op, cur_name]
                 else: pass
-                cur_length = cur_length - 1
-                cur_queue.append(syscall_kv)
+                if syscall_kv:
+                    cur_length = cur_length - 1
+                    cur_queue.append(syscall_kv)
+                else: 
+                    print("likely fsync error\n")
             init_size = init_size - 1
+            # logging.debug(f"cur_exist_file: {cur_exist_file}")
             logging.debug(f"cur_queue: {cur_queue}")
             SEED_QUEUE.put(cur_queue)
 

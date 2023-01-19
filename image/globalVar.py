04247fa49d2238b8e97f557e56490aa108fd47f0
erenphy <lixiaohui_ouc@163.com>
2023-01-19 01:22:19 -0800

    add reproduced testcases

--- image/globalVar.py
+++ image/globalVar.py
@@ -19,8 +19,8 @@ logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s 
 
 # 暂时设置超时时间为 4
 TIME_OUT = 10
-# 参数长度
-ARG_LENGTH = 3
+# 产生序列的最大长度
+ARG_LENGTH = 4
 
 # 文件系统镜像文件所在目录
 IMAGES_DIR = "images"
@@ -33,8 +33,30 @@ COUNT = 0
 # 测试用例队列
 TESTCASE_QUEUE = Queue()
 # TESTCASE_QUEUE = JoinableQueue()
-TESTCASE_QUEUE.put([['creat','./A/foo'],['sync'],['creat','A/bar'],['fsync','A']])
-TESTCASE_QUEUE.put([['hardlink',['foo','bar']],['sync'],['remove','bar'],['creat','bar'],['fsync','bar']])
+TESTCASE_QUEUE.put([['creat','./A/foo'],['sync'],['creat','./A/bar'],['fsync','./A']])
+TESTCASE_QUEUE.put([['hardlink',['./foo','./bar']],['sync'],['remove','./bar'],['creat','./bar'],['fsync','./bar']])
+TESTCASE_QUEUE.put([['write',['./foo','9']],['sync'],['fsync','./foo']])
+TESTCASE_QUEUE.put([['creat',['./A/foo']],['sync'],['creat','./A/bar'],['fsync','./A']])
+TESTCASE_QUEUE.put([['hardlink',['./foo','./bar']],['sync'],['remove','./bar'],['fsync','./foo']])
+TESTCASE_QUEUE.put([['creat','./A/foo'],['sync'],['rename', ['./A','./B']],['mkdir','./A'],['fsync','./A']])
+TESTCASE_QUEUE.put([['rename',['./foo','./bar']],['creat','./foo'],['fsync','./bar']])
+TESTCASE_QUEUE.put([['creat','./foo'],['fsync','./foo'],['write',['./foo','9']],['fsync','./foo']])
+TESTCASE_QUEUE.put([['hardlink',['./foo','./A/foo']], ['hardlink',['./bar','./A/bar']],['fsync','./bar']])
+# generic 107 
+TESTCASE_QUEUE.put([['hardlink',['./foo','./A/foo']],['hardlink',['./foo','./A/bar']],['sync'],['remove', './A/bar'], ['fsync','./foo']])
+# generic 321
+TESTCASE_QUEUE.put([['rename',['./foo','./A/foo']],['fsync','./A'],['fsync', './A/foo']])
+# generic 322
+TESTCASE_QUEUE.put([['rename',['./A/foo','./A/bar']],['fsync','./A/bar']])
+# generic 335
+TESTCASE_QUEUE.put([['rename',['./A/foo','./foo']],['creat','./bar'],['fsync','.']])
+# generic 336
+TESTCASE_QUEUE.put([['hardlink',['./A/foo','./B/foo']],['creat','./B/bar'],['sync'],['remove','./B/foo'],['rename',['./B/bar','C/bar']],['fsync','./A/foo']])
+# generic 343
+TESTCASE_QUEUE.put([['hardlink',['./A/foo','./A/bar']],['rename',['./B/baz','./A/baz'],['fsync', './A/foo']]])
+
+
+
 # 种子队列
 # SEED_QUEUE = JoinableQueue() 
 SEED_QUEUE = Queue() 
@@ -62,16 +84,19 @@ class ops(IntEnum):
 # 常量 一次赋值
 OPS_LENGTH = len(list(ops))
 DIR_FULL_SIZE = 15
-INIT_TIME_MAX = 20
-INIT_TIME_MIN = 10
+
+# 可修改，确定初始生成种子池的大小
+INIT_TIME_MAX = 11
+INIT_TIME_MIN = 6
+
 SMALL_DATA = 'abcdefgijklmnopqrst1234567890asdfghjkl;zxcvbnmqwertyuopifromLXH'
 BIG_DATA = 'a very very huge data size, waiting for appending, perheps from file'
 # 作为系统调用的参数：file/dir
-# MY_FILE_LIST = ['foo', 'bar', 'baz']
-MY_FILE_LIST = ['foo', 'bar']
+# MY_FILE_LIST = ['./foo', './bar', './baz']
+MY_FILE_LIST = ['./foo', './bar']
 MY_DIR_LIST = ['./', './A']
 
-MY_RECORD_LIST = Queue()
+# MY_RECORD_LIST = Queue()
 
 # 作为系统调用的参数：other
 MY_MNT_POINT = '/mnt/ext3' 

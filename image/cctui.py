03d421795e68dfe7d9c39cde4588eb9dc76b4a65
erenphy <lixiaohui_ouc@163.com>
2023-01-25 19:37:01 -0800

    add inputfield

--- image/cctui.py
+++ image/cctui.py
@@ -7,11 +7,9 @@ import globalVar
 from globalVar import *
 
 # TODO 移动到 globalVar 里头去
-START_TIME = datetime.now().timestamp()
-LAST_NEW_PATH_TIME = None
-LAST_CRASH_TIME = None
-TOTAL_PATHS = 0
-UNIQ_CRASHES = 0
+# START_TIME = datetime.now().timestamp()
+# TOTAL_PATHS = 0
+# UNIQ_CRASHES = 0
 
 def macro_time(start) -> str:
     if start == "None":
@@ -42,19 +40,20 @@ def start_tui(thread_list):
                     "",
                 ),
                 Container(
-                    Label("[bold accent]process timing"),
+                    Label("[bold accent]Process timing"),
                     "",
-                    InputField(f"[!time]{START_TIME}", prompt="run time: "),
-                    InputField(f"[!time]{LAST_NEW_PATH_TIME}", prompt="last new path: "),
-                    InputField(f"[!time]{LAST_CRASH_TIME}", prompt="last uniq crash: "),
+                    InputField("[!time]{}".format(globalVar.get_value("START_TIME")), prompt="run time: "),
+                    #InputField(f"[!time]{LAST_NEW_PATH_TIME}", prompt="last new path: "),
+                    InputField("[!time]{}".format(globalVar.get_value("LAST_CRASH_TIME")), prompt="last uniq crash time: "),
                     "",
                 ),
                 Container(
                     Label("[bold accent]overall results"),
                     "",
-                    InputField(f"{TOTAL_PATHS}", prompt="total paths: "),
-                    InputField(f"{UNIQ_CRASHES}", prompt="uniq crashes: "),
-                    InputField(f"{CSV_NAME}", prompt="report path: "),
+                    InputField(str(globalVar.get_value("SEED_COUNT")), prompt="Seedpool size: "),
+                    InputField(str(globalVar.get_value("TOTAL_PATH")), prompt="total paths: "),
+                    InputField(str(globalVar.get_value("UNIQ_CRASHES")), prompt="uniq crashes: "),
+                    InputField(globalVar.get_value("CSV_NAME"), prompt="report path: "),
                     "",
                 ),
                 "",
@@ -62,8 +61,8 @@ def start_tui(thread_list):
                 width=80,
                 box="DOUBLE",
             )
-            .set_title("[110 bold]fuzzcc")
+            .set_title("[110 bold]FuzzCC")
             .center()
         )
-
         manager.add(window)
+        # manager.add(window)
